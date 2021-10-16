68ff041c3142b1fd40616010469e5e52e2594594
Leonardo Prevost <prevostleonardo@gmail.com>
2021-10-15 21:35:09 -0300

    added request API

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-document.querySelector(".search").addEventListener("submit", (e)=>{
+document.querySelector(".search").addEventListener("submit", async(e)=>{
     e.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+
+    if(input != ''){
+        showWarning('Loading...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=785817fb5bbcfac20498b118dbb250e0&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.code === 200) {
+
+        }else {
+            showWarning('Não encontramos esta localização!')
+        }
+    } 
 });
+
+
+function showWarning(msg){
+   document.querySelector('.warning').innerHTML = msg;
+
+} 
+
