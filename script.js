b4573e8c3394317fe4ce5b3ddf53b6fd0bb098e4
Leonardo Prevost <prevostleonardo@gmail.com>
2021-11-12 17:38:43 -0300

    add clear info

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector(".search").addEventListener("submit", async(e)=>{
     let input = document.querySelector('#searchInput').value;
 
     if(input != ''){
+        clearInfo();
         showWarning('Loading...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=785817fb5bbcfac20498b118dbb250e0&units=metric&lang=pt_br`;
